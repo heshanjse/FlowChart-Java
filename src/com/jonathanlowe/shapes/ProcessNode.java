5b08c58a6c33a79c9e7c35c7785b5b21dfd79c8f
Jonathan Lowe <jlowe258@hotmail.com>
2014-05-31 19:41:22 -0700

    This is the first forked commit.

--- src/com/jonathanlowe/shapes/ProcessNode.java
+++ src/com/jonathanlowe/shapes/ProcessNode.java
@@ -3,6 +3,7 @@ package com.jonathanlowe.shapes;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Polygon;
+import java.awt.event.MouseEvent;
 
 public class ProcessNode extends FlowChartNode {
 	protected Polygon poly;
@@ -15,13 +16,20 @@ public class ProcessNode extends FlowChartNode {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		int xPoly[] = {0,0,200,200};
-		int yPoly[] = {0,200,200,0};
+		int xPoly[] = {0,200,200,0};
+		int yPoly[] = {0,0,200,200};
 		poly = new Polygon(xPoly, yPoly, xPoly.length);
-		g.setColor(Color.RED);
-		
-		g.fillPolygon(poly);
-		
+		g.setColor(Color.RED);		
+		g.fillPolygon(poly);		
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		pt = e.getPoint();
+		isClickInShape = poly.contains(pt);
+		System.out.println(pt.x);
+		System.out.println("This works");
+		// TODO Auto-generated method stub
 		
 	}
 }
